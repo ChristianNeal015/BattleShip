2c9a7a0bedf29e4b6a3c9116efe6a1de7bf84526
ChristianNeal015 <cjdaking030@gmail.com>
2019-10-21 12:59:26 -0400

    Update game

---
+++ src/main/java/simpleBattleShip/SimpleDotComTestDrive.java
@@ -0,0 +1,11 @@
+package simpleBattleShip;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        DotCom dot = new DotCom();
+        int [] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+        String userGuess = "2";
+        String result = dot.checkYourself(userGuess);
+    }
+}
